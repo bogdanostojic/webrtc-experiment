e12b8b8b0401a10545d0a2577c1cb0ae631acc1c
Bogdan Ostojic <bogdanostojic1993@gmail.com>
2019-06-20 23:11:10 +0200

    testing webhook for discord

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ console.log('Server is running on port :9090');
 var io = require('socket.io')(server);
 
 io.on('connection', newConnection);
-
+console.log('checkout!')
 function newConnection(socket) {
     console.log(socket.id)
 }
